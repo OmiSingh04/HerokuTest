c30bc5349502adf4ea8a22cbee5b262c9aca5b66
OmiSingh2004 <omisinghdbz@gmail.com>
2021-09-08 16:19:56 +0530

    bruh

---
+++ src/main/java/bot/test/App.java
@@ -0,0 +1,34 @@
+package bot.test;
+
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import javax.security.auth.login.LoginException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends ListenerAdapter {
+    public static void main(String[] args) throws LoginException {
+        JDABuilder builder = JDABuilder.createDefault(System.getenv("TOKEN"));
+        builder.setActivity(Activity.watching("TV"));
+        builder.addEventListeners(new App());
+        builder.build();
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        if (event.getMessage().getContentRaw().startsWith("!ping"))
+        {
+            long time = System.currentTimeMillis();
+            event.getChannel().sendMessage("Pong!") /* => RestAction<Message> */
+                    .queue(response /* => Message */ -> {
+                        response.editMessageFormat("Pong: %d ms", System.currentTimeMillis() - time).queue();
+                    });
+        }
+    }
+}
